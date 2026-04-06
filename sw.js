160f6a94331ec814edfa25c7446fee7831dd425d
sowanlove0930-a11y <sowanlove0930@gmail.com>
2026-04-06 19:56:52 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'unice-v1';
+const CACHE = 'unice-v3';
 const ASSETS = ['/fp.html', '/icon-192.png', '/icon-512.png', '/manifest-fp.json'];
 
 self.addEventListener('install', e => {
@@ -7,6 +7,7 @@ self.addEventListener('install', e => {
 });
 
 self.addEventListener('activate', e => {
+  // 이전 버전 캐시 전부 삭제
   e.waitUntil(caches.keys().then(keys =>
     Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
   ));
@@ -14,6 +15,13 @@ self.addEventListener('activate', e => {
 });
 
 self.addEventListener('fetch', e => {
+  // fp.html은 항상 네트워크에서 최신 버전 가져옴 (캐시 우선 X)
+  if (e.request.url.includes('fp.html')) {
+    e.respondWith(
+      fetch(e.request).catch(() => caches.match(e.request))
+    );
+    return;
+  }
   e.respondWith(
     caches.match(e.request).then(r => r || fetch(e.request))
   );
