6530c913fe7f3fd87044637d2323705d37a0daa7
sowanlove0930-a11y <sowanlove0930@gmail.com>
2026-09-07 12:32:30 +0900

    Add files via upload

--- unispace/sw.js
+++ unispace/sw.js
@@ -56,7 +56,7 @@ self.addEventListener('notificationclick', function(event) {
 });
 
 // ===== PWA cache (network-first) =====
-var CACHE_NAME = 'unispace-v74';
+var CACHE_NAME = 'unispace-v76';
 var PRECACHE = [
   './manifest.json',
   './unispace-192.png',
